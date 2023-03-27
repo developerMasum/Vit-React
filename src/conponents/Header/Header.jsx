375cd00ca7a5156927e5dda0b27db506244a81c4
developerMasum <developer.masum2001@gmail.com>
2023-03-27 08:12:29 +0600

    header added

---
+++ src/conponents/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+            <a href="/Shop">Shop</a>
+            <a href="/Order">Order</a>
+            <a href="/Inventory">Inventory</a>
+            <a href="/LogIN">LogIN</a>
+            </div>
+
+           
+        
+        </nav>
+    );
+};
+
+export default Header;
