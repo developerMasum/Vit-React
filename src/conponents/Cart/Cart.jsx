0e8c4b41de2e0a6185ca4706446e77702c0836a4
developerMasum <developer.masum2001@gmail.com>
2023-03-28 13:16:03 +0600

    finnal version

--- src/conponents/Cart/Cart.jsx
+++ src/conponents/Cart/Cart.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = ({cart}) => {
-    console.log(cart);
+    
   // const cart = props.cart;  //opn -1
   // const{cart} = props; opn-2
 
 // total price + shipping
 let total = 0;
 let totalShipping = 0;
+let quantity = 0;
  for (const product of cart) {
-    total = total + product.price
+
+    // if(product.quantity === 0){
+    //     product.quantity = 1;
+    // shotcut is here ...
+    // product.quantity = product.quantity || 1 ;
+
+    total = total + product.price *product.quantity;
     totalShipping = totalShipping + product.shipping ;  
+    quantity = quantity + product.quantity;
 }  
 
 // tax -7% 
@@ -20,7 +28,7 @@ const grandTotal = total + tax;
   return (
     <div className="cart">
       <h2>Cart Summary </h2>
-      <h4>Added Items:{cart.length}</h4>
+      <h4>Added Items:{quantity}</h4>
       <p>Total Price:${total.toFixed(2)} </p>
       <p>Total Shipping Charge:${totalShipping}</p>
       <p>Tax:${tax.toFixed(2)}</p>
